8662777b90e0801e11b5f91feb1a873d85e15609
deepakj782 <87135643+deepakj782@users.noreply.github.com>
2022-01-23 11:50:26 +0530

    Update Merchant1.java

--- Mini Application/Amazon/Merchant1.java
+++ Mini Application/Amazon/Merchant1.java
@@ -1,14 +1,16 @@
-
-
 public class Merchant1 {
     
-       String name;
+     static  String name;
        String password;  
        String status="pending";
 
-    
+    public Merchant1(String name, String password) 
+       {
+           this.name = name;
+           this.password = password;
+       } 
 
-    public Merchant1(String name, String password) {
+    public Merchant1(String name, String password, String status) {
         this.name = name;
         this.password = password;
         this.status = status;  
@@ -24,7 +26,8 @@ public class Merchant1 {
                 case 1:
                     signin();
                 case 2:
-
+                    signup();
+                    
                 case 3:
                 //Main1.public static void main(String[] args) {
                     
@@ -42,21 +45,51 @@ public class Merchant1 {
     public static void signin(){
         System.out.println("Welcome to New user  ");
 
-        System.out.println("Enter your name ");
+        System.out.println("Enter user name ");
         String name=Main1.sc.nextLine();
+
         System.out.println("Enter password ");
         String password=Main1.sc.nextLine();
+
         Main1.merchants.add(new Merchant1(name, password));
-        System.out.println("Thanks for Signing Up ! Your Request is Pending for Approval");
+
+        System.out.println("Thanks for Signing Up ! Your Request is waiting for Approval");
         System.out.println("Press Enter to continue");
         Main1.sc.nextLine();
-        Main1.home();
+         Main1.home();
         
 
     }
 
     
+    public static void signup() {
+        
+        System.out.println("\033[H\033[2J");
+        System.out.print("Enter User name : ");
+        String username = Main1.sc.nextLine();
+        System.out.print("Enter Password : ");
+        String password =Main1.sc.nextLine();
 
+        if(username.equals(name) && password.equals(password))
+        {
+            System.out.println("\033[H\033[2J");
+            System.out.println("User logged in succesfully....");
+            Main1.sc.nextLine();
+            //product();
+
+        }
+        else
+        {
+            System.out.println("\033[H\033[2J");
+            System.out.println("Invalid input !!!");
+            Main1.sc.nextLine();
+            merchantlogin();
+            
+        }
     }
-    
 
+        
+            
+    }
+
+    
