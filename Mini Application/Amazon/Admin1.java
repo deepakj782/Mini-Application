d1eb3a369058367639de7dbc2c74ba71bd4a2958
deepakj782 <87135643+deepakj782@users.noreply.github.com>
2022-01-23 11:49:15 +0530

    Update Admin1.java

--- Mini Application/Amazon/Admin1.java
+++ Mini Application/Amazon/Admin1.java
@@ -1,9 +1,9 @@
 
-
-public class Admin1 {  
+ 
+public class Admin1 {
     
     public static void Login(){
-        System.out.println("____Welcome to admin login____");
+        System.out.println("Welcome to admin login");
         System.out.println("Enter user name");
         String name=Main1.sc.nextLine();
         System.out.println("Enter password");
@@ -11,7 +11,11 @@ public class Admin1 {
         if(name.equals("admin") && password.equals("admin")){
             adminpage();
             
-  
+      
+        }
+        else{
+            System.out.println("Inavlid user name/password ");
+            Login();
         }
     }
 
@@ -37,31 +41,35 @@ public class Admin1 {
         }
     }
 
-    public static void approval() {
-        for (int i = 0; i < Main1.merchants.size(); i++) {
-             System.out.print("\033[H\033[2J");
-             
-            if (Main1.merchants.get(i).status.equals("Pending")) {
-                System.out.println("User : " + Main1.merchants.get(i).name);
-                System.out.println("1 Approval \n  Any other number for Rejection :");
+    public static void approval() 
+    {
+        for (int i = 0; i < Main1.merchants.size(); i++, System.out.print("\033[H\033[2J")) 
+        {
+            if (Main1.merchants.get(i).status.equals("pending")) 
+            {
+                System.out.println("\033[H\033[2J");
+                System.out.println("User name : " + Main1.merchants.get(i).name);
+                System.out.print("Press 1 for Approve (OR) Any other number for Rejection : ");
                 int n = Integer.parseInt(Main1.sc.nextLine());
-                if (n == 1) {
+                if (n == 1) 
+                {
                     Main1.merchants.get(i).status = "Approved";
+                    System.out.println("\033[H\033[2J");
                     System.out.printf("Merchant \"%s\" has been Approved\n", Main1.merchants.get(i).name);
+                    System.out.println();
                     System.out.println("Press enter to continue");
                     Main1.sc.nextLine();
                     continue;
-                 } else {
+                } else 
+                {
                     Main1.merchants.get(i).status = "Rejected";
                     System.out.println("User Rejected");
                     System.out.println("Press enter to continue");
                     Main1.sc.nextLine();
                 }
+                System.out.println("Merchants to be Approve :- 0");
             }
         }
-        System.out.println("Merchants Waiting for Approval :- 0");
         adminpage();
     }
-
-    
 }
